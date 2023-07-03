b303e81b1d82af84e21230fda6b03612121196b7
NotTolerable <121141694+NotTolerable@users.noreply.github.com>
2023-07-03 13:29:03 -0400

    Update FastPotion.java

--- FastPot/src/me/lasillje/fastpot/FastPotion.java
+++ FastPot/src/me/lasillje/fastpot/FastPotion.java
@@ -1,19 +1,50 @@
 package me.lasillje.fastpot;
 
-import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerItemDamageEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
-import me.lasillje.fastpot.listeners.PotionListener;
+public class FastPotion extends JavaPlugin implements Listener {
 
-public class FastPotion extends JavaPlugin {
+        @Override
+        public void onEnable() {
+                getServer().getPluginManager().registerEvents(this, this);
+                }
 
-	@Override
-	public void onEnable() {
-		registerListener(new PotionListener());
-	}
-	
-	public void registerListener(Listener listener) {
-		Bukkit.getPluginManager().registerEvents(listener,this);
-	}
+        @EventHandler
+        public void onPlayerDamage(EntityDamageByEntityEvent event) {
+                if (event.getEntity() instanceof Player) {
+                        Player damagedPlayer = (Player) event.getEntity();
+                        disableShield(damagedPlayer);
+                }
+        
+                if (event.getDamager() instanceof Player) {
+                        Player damager = (Player) event.getDamager();
+                        disableShield(damager);
+                }
+        }
+
+        @EventHandler
+        public void onPlayerItemDamage(PlayerItemDamageEvent event) {
+                if (event.getItem().getType() == Material.SHIELD) {
+                        event.setCancelled(true);
+                }
+        }
+
+        private void disableShield(Player player) {
+                player.getInventory().setItemInOffHand(null); // Clear off-hand item
+        
+                new BukkitRunnable() {
+                        @Override
+                        public void run() {
+                                player.getInventory().setItemInOffHand(player.getInventory().getItemInMainHand()); // Restore off-hand item
+                        }
+                }.runTaskLater(this, 20L); // Delay for 1 second (20 ticks)
+        }
 }
